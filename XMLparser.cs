e49e5ed8d2a915b8958d961a091f48e74b08de80
jp_prfsn <jpb3rz@gmail.com>
2023-04-07 13:42:25 +0100

    adds settings / title / path validation

--- XMLparser.cs
+++ XMLparser.cs
@@ -4,28 +4,143 @@ using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using System.Threading; 
+
 public class Operator
 {
 
     public static string[] xmlFiles;
+    public static string filePathToRead;
+    public static string outputLocation;
 
-    public static void pullIt(string fp, string ol){
+    // delimiter
+    public static string delim = ";";
+
+    public static List<string> exclusions = new List<string>();
+
+   
+    public static void Main()
+    {
+        //CREATE TEXT FILE
+        Console.WriteLine(" ");
+        Console.WriteLine("Rimworld XML Data Extractor");
+        Console.WriteLine("[@patchware] // 2023");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("https://github.com/jp-prfsn/Rimworld-XML-Data-Extractor");
+        Console.WriteLine("----------------------------");
+        
+
+        getMode();
+
+        
+
+    }
+
+    public static void getMode(){
+
+        Console.WriteLine(" ");
+        Console.WriteLine("type 'new' to run an extraction");
+        Console.WriteLine("type 'settings' to change settings");
+        Console.WriteLine("type 'exit' to close program");
+
+        Console.WriteLine("----------------------------");
+
+        string opt = Console.ReadLine();
+        if(opt == "new"){
+            Console.WriteLine("Folder location of defs to read:");
+            Console.WriteLine("[ Submit blank to use default of ]");
+            Console.WriteLine("[ C:\Program Files (x86)\Steam\steamapps\common\RimWorld ]");
+            filePathToRead = getPath("C:\Program Files (x86)\Steam\steamapps\common\RimWorld");
+
+            Console.WriteLine("Enter file output location: ");
+            Console.WriteLine("[ Submit blank to use default of Desktop ]");
+            outputLocation = getPath("Environment.GetFolderPath(Environment.SpecialFolder.Desktop)");
+
+            Console.WriteLine("Finding files...");
+
+            xmlFiles = Directory.GetFiles(filepath, "*.xml", SearchOption.AllDirectories);
+            pullIt();
+
+        }else if(opt == "settings"){
+            Console.WriteLine(" ");
+            Console.WriteLine("[1] change delimiter");
+            Console.WriteLine("[2] add exclusions");
+            Console.WriteLine("[3] remove exclusions");
+            Console.WriteLine("[4] exit to menu");
+            Console.WriteLine("----------------------------");
+            string setter = Console.ReadLine();
+            if(setter == "1"){
+                // CHANGE A DELIMITER
+                Console.WriteLine("Preferred delimiter:");
+                delim = Console.ReadLine();
+
+            }else if(setter == "2"){
+
+                // ADD AN EXCLUSION TERM
+                Console.WriteLine("Current exclusions:");
+                foreach(string e in exclusions){
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine("New exclusion:");
+                exclusions.Add(Console.ReadLine());
+
+            }else if(setter == "3"){
+
+                // ADD AN EXCLUSION TERM
+                Console.WriteLine("Current exclusions:");
+                foreach(string e in exclusions){
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine("New exclusion:");
+                exclusions.Remove(Console.ReadLine());
+
+            }
+            
+
+            getMode();
+
+        }else if(opt == "exit"){
+            System.Environment.Exit(1);
+        }else{
+            Console.WriteLine("No such command.");
+
+            getMode();
+        }
+    }
+
+    public static string getPath(string default){
+        path = Console.ReadLine();
+        if(path == ""){
+            return default;
+        }
+        else if(Directory.Exists(path))
+        {
+            Console.WriteLine("Path OK.");
+            return path;
+        }else{
+            Console.WriteLine("Path not found. Try again.");
+            return getPath();
+        }
+    }
+
+    public static void pullIt(){
         //CREATE TEXT FILE
 
         Console.WriteLine("Which tag are you interested in?");
+        Console.WriteLine("[ e.g. ThingDef, ResearchProjDef ]");
+
         string tagLabel = Console.ReadLine();
 
         Console.WriteLine("Which properties of that tag do you want? (comma separated values, no spaces)");
+        Console.WriteLine("[ e.g. label,baseCost,prequisites ]");
+
         string propLong = Console.ReadLine(); 
         string[] props = propLong.Split(',');
-        foreach(string p in props){
-            Console.WriteLine(p);
-        }
 
-
-        using (StreamWriter outputFile = File.CreateText(ol + @"\" + tagLabel + ".txt"))
+        using (StreamWriter outputFile = File.CreateText(outputLocation + @"\" + tagLabel + ".txt"))
         {
-            xmlFiles = Directory.GetFiles(fp, "*.xml", SearchOption.AllDirectories);
+            xmlFiles = Directory.GetFiles(filePathToRead, "*.xml", SearchOption.AllDirectories);
             Console.WriteLine("Pulling " + tagLabel + "s");
             Console.WriteLine("XML Files found: " + xmlFiles.Length);
             Console.WriteLine("....................................");
@@ -39,57 +154,67 @@ public class Operator
                 string thisModName = getModName(file); 
 
                 // filter out files that give issues or are not important
-                if(file.Contains("Languages")){
-                    continue;
-                } try {
+                foreach(string e in exclusions)
+                    if(file.Contains(e)){
+                        continue;
+                    }
+                }
+                
+                
+                try {
                     // OPEN THE FILE
-                    XmlDocument xmlDoc= new XmlDocument();              // Create an XML document object ;
-                    xmlDoc.Load(file);                                  // Load the XML document from the specified file ;
+                    XmlDocument xmlDoc= new XmlDocument();
+                    xmlDoc.Load(file);
 
 
                     // FOR EACH HEADNODE
-                    XmlNodeList defs = xmlDoc.GetElementsByTagName(tagLabel); // get list of ResearchProjectDef
+                    XmlNodeList defs = xmlDoc.GetElementsByTagName(tagLabel);
 
-                    if(defs.Count == 0){
-                        //Console.WriteLine( "No ResearchProjectDef found in this file.");
-                    }else{
+                    if(defs.Count > 0){
                         for(int k=0; k < defs.Count; k++){
                             XmlNode thisNode = defs[k];
                             string oneLine = "";
 
-                            if (thisNode.HasChildNodes)                                             // if this node has children
+                            if (thisNode.HasChildNodes) // if this node has children
                             {
-                                XmlNodeList properties = thisNode.ChildNodes;
-
                                 // for each property we mentioned
                                 foreach (string p in props){
 
                                     XmlNode property = thisNode[p];
-                                    // if it has children (that are not text... WHAT?!?!)
-                                    if (property.HasChildNodes && property.ChildNodes[0].NodeType != XmlNodeType.Text)
-                                    {
-                                    
-                                        //list them separated by comma
-                                        foreach (XmlNode c in property){
-                                            oneLine += c.InnerText + ",";
+                                    if(property != null){
+                                        if(!property.HasChildNodes){
+                                            oneLine +=  "0" + delim;
                                         }
-                                        oneLine += ";";
+                                        else if (property.ChildNodes[0].NodeType != XmlNodeType.Text)
+                                        {
                                         
-                                        
-                                    }
-                                    else{
-                                        //else sparate by semicolon
-                                        oneLine += property.InnerText + ";";
+                                            //list them separated by comma
+                                            foreach (XmlNode c in property){
+                                                oneLine += c.InnerText + ",";
+                                            }
+                                            oneLine += delim;
+                                            
+                                            
+                                        }
+                                        else{
+                                            //else sparate by semicolon
+                                            oneLine += property.InnerText + delim;
+                                        }
+                                    }else{
+                                        //Handle unfound tags
+                                        oneLine +=  "0" + delim;
                                     }
                                 }
+                                oneLine += thisModName;
                             }
                             
-                            string formatString = oneLine.TrimEnd(';');
+                            string formatString = oneLine.TrimEnd(delim);
                             formatString = formatString.TrimEnd(',');
                             outputFile.WriteLine(formatString);
                         } 
                     } 
                 } catch (Exception ex){
+                    Console.WriteLine(ex);
                     continue;
                 }
 
@@ -104,229 +229,13 @@ public class Operator
         Console.WriteLine("Another one? (Y/N)");
         string contin = Console.ReadLine();
         if(contin == (string)"Y" || contin == (string)"y"){
-            pullIt(fp, ol);
+            getMode();
         }else{
             Console.WriteLine("OK BYE. Press RETURN key to exit.");
             Console.ReadLine(); 
         }
 
     }
-
-   
-    public static void Main()
-    {
-        //CREATE TEXT FILE
- 
-        Console.WriteLine("HI FREN!");
-        Console.WriteLine("Enter the top level directory containing your defs: ");
-        string filepath = Console.ReadLine(); 
-
-        Console.WriteLine("Enter file output location: ");
-        string outputLocation = Console.ReadLine(); 
-
-
-        Console.WriteLine("Cheers. ");
-
-        xmlFiles = Directory.GetFiles(filepath, "*.xml", SearchOption.AllDirectories);
-        pullIt(filepath, outputLocation);
-
-        
-
-
-/*
-        using (StreamWriter outputFile = File.CreateText(outputLocation + @"\researchProjDefs.txt"))
-        {
-            
-            Console.WriteLine("Pulling researchProjDefs");
-            Console.WriteLine("XML Files found: " + xmlFiles.Length);
-            Console.WriteLine("....................................");
-            int count = 1;
-            float perc = 0;
-            foreach(string file in xmlFiles){
-
-                perc = ((float)count/xmlFiles.Length) * 100;
-                Console.Write("\r{0}%   ", perc);
-
-                
-                
- 
-                count++; 
-
-                string thisModName = getModName(file); 
-
-                // filter out files that give issues or are not important
-                if(file.Contains("Languages")){
-                    continue;
-                } try {
-                    // OPEN THE FILE
-                    //Console.WriteLine("....................................");
-                    //Console.WriteLine("Processing file: " + file);
-                    XmlDocument xmlDoc= new XmlDocument();              // Create an XML document object ;
-                    xmlDoc.Load(file);                                  // Load the XML document from the specified file ;
-
-
-                    // FOR EACH HEADNODE
-                    XmlNodeList rpDefs = xmlDoc.GetElementsByTagName("ResearchProjectDef"); // get list of ResearchProjectDef
-
-                    if(rpDefs.Count == 0){
-                        //Console.WriteLine( "No ResearchProjectDef found in this file.");
-                    }else{
-                        for(int k=0; k < rpDefs.Count; k++){
-                            XmlNode thisNode = rpDefs[k];
-                            //Console.WriteLine("[ " + thisNode.defName.InnerText + " ]");
-                            string oneLine = "";
-
-                            if (thisNode.HasChildNodes)                                             // if this node has children
-                            {
-
-                                XmlNodeList properties = thisNode.ChildNodes;
-                                //Console.WriteLine(thisNode.Name + " has " + properties.Count + " child nodes!");
-
-                                
-                                string defName = thisNode["defName"].InnerText;
-                                isNull(defName);
-
-                                
-                                string baseCost = thisNode["baseCost"].InnerText;
-                                isNull(baseCost);
-
-                                string techLevel = thisNode["techLevel"].InnerText;
-                                isNull(techLevel);
-
-                                XmlNodeList prerequisites = thisNode["prerequisites"].ChildNodes;
-
-                                
-                                oneLine = oneLine + defName + ";" + baseCost + ";" + techLevel + ";" + thisModName + ";";
-
-                                foreach(XmlNode prq in prerequisites){
-                                    oneLine += prq.InnerText + ",";
-                                }
-
-                                //Console.WriteLine(defName + ";" + baseCost + ";" + techLevel);
-                                    
-                                
-                       
-                            }
-                            
-                            string formatString = oneLine.TrimEnd(';');
-                            formatString = formatString.TrimEnd(',');
-                            outputFile.WriteLine(formatString);
-                             
-
-                            //Console.WriteLine("---- " + oneLine.TrimEnd(';'));
-                        } 
-                    } 
-                } catch (Exception ex){
-                    continue;
-                }
-
-                
-            }
-
-            outputFile.Close();
-            Console.WriteLine("....................................");
-        }
-
-
-
-
-
-
-        // step 2 - export ThingDefs
-
-        using (StreamWriter outputFile = File.CreateText(outputLocation + @"\thingDefs.txt"))
-        {
-            Console.WriteLine("Pulling THINGDEFS");
-            Console.WriteLine("XML Files found: " + xmlFiles.Length);
-            Console.WriteLine("....................................");
-
-            int count = 1;
-            float perc = 0;
-            foreach(string file in xmlFiles){
-
-                perc = ((float)count/xmlFiles.Length) * 100;
-                Console.Write("\r{0}%   ", perc);
-                count++;
-                string thisModName = getModName(file); 
-
-
-                // filter out files that give issues or are not important
-                if(file.Contains("Languages")){
-                    continue;
-                } try {
-                    // OPEN THE FILE
-                    //Console.WriteLine("....................................");
-                    //Console.WriteLine("Processing file: " + file);
-                    XmlDocument xmlDoc= new XmlDocument();              // Create an XML document object ;
-                    xmlDoc.Load(file);                                  // Load the XML document from the specified file ;
-
-
-                    // FOR EACH HEADNODE
-                    XmlNodeList thingDefs = xmlDoc.GetElementsByTagName("ThingDef"); // get list of ResearchProjectDef
-
-                    if(thingDefs.Count == 0){
-                        //Console.WriteLine( "No thingDefs found in this file.");
-                    }else{
-                        for(int m=0; m < thingDefs.Count; m++){
-                            XmlNode thisNode = thingDefs[m];
-                            //Console.WriteLine("[ " + thisNode.defName.InnerText + " ]");
-                            string oneLine = "";
-
-                            if (thisNode.HasChildNodes)                                             // if this node has children
-                            {
-
-                                XmlNodeList properties = thisNode.ChildNodes;
-                                //Console.WriteLine(thisNode.Name + " has " + properties.Count + " child nodes!");
-
-                                
-                                string label = thisNode["label"].InnerText;
-                                isNull(label);
-
-                                XmlNodeList prerequisites = thisNode["researchPrerequisites"].ChildNodes;
-
-                                
-                                oneLine = oneLine + label + ";" + thisModName + ";";
-
-                                foreach(XmlNode prq in prerequisites){
-                                    oneLine += prq.InnerText + ",";
-                                }
-
-                                //Console.WriteLine(label);
-                                    
-    
-                            }
-                            
-                            string formatString = oneLine.TrimEnd(';');
-                            formatString = formatString.TrimEnd(',');
-                            outputFile.WriteLine(formatString);
-                             
-
-                            //Console.WriteLine("---- " + oneLine.TrimEnd(';'));
-                        } 
-                    } 
-                } catch (Exception ex){
-                    continue;
-                }  
-            }
-
-            outputFile.Close(); 
-        }
-
-        */
-        
-    
-    }
-
-     public static bool isNull(string word){
-        if(word != null){
-             //Console.WriteLine(word + " found");
-            return false;
-        }else{
-            //Console.WriteLine(word + " could NOT be found");
-            return true;
-        }
-    }
-
 
     public static string getModName(string fl){
 
